fcaf95dc444d04e5472c672c1743392a2c20df76
Parz <Parz90@users.noreply.github.com>
2017-01-01 19:41:24 +0100

    Test Code for MLZ

--- MLZ/1_3_GoogleMaps/GoogleMaps.js
+++ MLZ/1_3_GoogleMaps/GoogleMaps.js
@@ -4,11 +4,61 @@ Date: 02. January 2017
 Version: v5.0
 */
 
-function myMap() {
-  var mapCanvas = document.getElementById("map");
-  var mapOptions = {
-    center: new google.maps.LatLng(51.5, -0.2), 
-    zoom: 10
-  }
-  var map = new google.maps.Map(mapCanvas, mapOptions);
+/*
+var location1 = https: //maps.googleapis.com/maps/api/geocode/json?
+    address = 1600 + Amphitheatre + Parkway,
+    +Mountain + View, +CA & key = AIzaSyA0ZkIjgtggbIkGyLGCzSpNuIbd8t1x7BE;
+*/
+
+function createMarker(latlng) {
+
+    // If the user makes another search you must clear the marker variable
+    if (marker != undefined && marker != '') {
+        marker.setMap(null);
+        marker = '';
+    }
+
+    marker = new google.maps.Marker({
+        map: map,
+        position: latlng
+    });
+
 }
+
+function googleMap() {
+    var mapCanvas = document.getElementById('map');
+    var myPos = new google.maps.LatLng(47.349, 8.7065);
+    var mapOptions = {
+        center: myPos,
+        zoom: 10
+    };
+    var map = new google.maps.Map(mapCanvas, mapOptions);
+
+    var marker = new google.maps.Marker({
+        position: myPos,
+        map: map,
+        title: 'HBU Uster, Berufsschulstrasse 1, 8610 Uster'
+    })
+    };
+    
+    
+    
+    
+    var addressInput = "Dorfstrasse 10, 8630 Rüti, Switzerland";
+    var geocoder = new google.maps.Geocoder();
+
+    geocoder.geocode({
+            address: addressInput
+        }, function (results, status) {
+
+            if (status == google.maps.GeocoderStatus.OK) {
+
+                var myResult = results[0].geometry.location; // reference LatLng value
+
+                createMarker(myResult); // call the function that adds the marker
+
+                map.setCenter(myResult);
+
+                map.setZoom(17);
+            };
+    })
